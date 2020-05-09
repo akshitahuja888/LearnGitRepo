c4b66f1e82208750014723fe262503056f971c00
akshitahuja888 <akshitahuja888@gmail.com>
2020-05-09 16:39:54 +0530

    for loops

---
+++ for_loops.cpp
@@ -0,0 +1,27 @@
+  
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <cstdio>
+#include <cstring>
+#include <stack>
+#include <iomanip>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+
+	for(int i=0;i<10;i++)
+	{
+		cout<<"git"<<endl;
+	}
+
+	return 0;
+}
